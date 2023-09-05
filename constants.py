4242ea17db9c4a31b58ec0da36d1f1da8e5b8ac2
Benjamin Owen <benjamin@projecttiy.com>
2023-09-05 09:50:28 -0400

    added in a new file called constants.py that holds all constants used by any class though right now it is constants used only by the user class, adjusted the user class to use the new constants file, adjusted the main.py script to use the new constants, adjusted the version of the program in main.py, adjusted some wording of the description in main.py

---
+++ constants.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+COUNTRIES_WITH_STATES = [
+    "United States of America", "Australia", "Canada", "India"
+]
+
+VALID_COUNTRIES = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda",
+    "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh",
+    "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia-Herzegovina",
+    "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria",
+    "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
+    "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia",
+    "Comoros", "Congo", "Cook Islands", "Costa Rica", "Cote D'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic",
+    "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+    "Equatorial Guinea", "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands (Malvinas)", "Fiji", "Finland",
+    "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia",
+    "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guatemala", "Guernsey, C.I.",
+    "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and McDonald Islands", "Honduras", "Hong Kong",
+    "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Isle of Man", "Israel",
+    "Italy", "Jamaica", "Japan", "Jersey, C.I.", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea", "Kuwait",
+    "Kyrgyzstan", "Lao Peoples Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia",
+    "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar",
+    "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius",
+    "Mexico", "Micronesia, Fed. States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco",
+    "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherland Antilles", "Netherlands",
+    "Neutral Zone (Saudi/Iraq)", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue",
+    "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama Canal Zone",
+    "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico",
+    "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Samoa", "San Marino",
+    "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovak Republic",
+    "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "St. Helena",
+    "St. Pierre and Miquelon", "St. Vincent and the Grenadines", "Sudan", "Suriname",
+    "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan",
+    "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago",
+    "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "U.S.Minor Outlying Islands",
+    "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay",
+    "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Vietnam", "Virgin Islands (British)",
+    "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe"
+]
+
+VALID_US_STATES = [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "Armed Forces Asia", "Armed Forces Europe", "Armed Forces Pacific",
+    "California", "Colorado", "Connecticut", "Delaware", "District of Columbia","Florida", "Georgia", "Hawaii",
+    "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+    "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+    "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+    "Pennsylvania", "Rhode Island","South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+]
+
+VALID_AUS_STATES = [
+    "New South Wales", "Northern Territory", "Queensland", "Tasmania", "Victoria", "Western Australia",
+]
+
+VALID_CAN_STATES = [
+    "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland",
+    "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island", "Quebec",
+    "Saskatchewan", "Yukon"
+]
+
+VALID_IN_STATES = [
+    "Andhra Pradesh", "Andaman and Nicobar Islands", "Arunachal Pradesh", "Assam", "Bihar",
+    "Chandigarh", "Chattisgarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana",
+    "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshadadweep",
+    "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Orissa", "Pondicherry",
+    "Punjab", "Rajasthan","Sikkim", "Tamil Nadu", "Tripura", "Uttar Pradesh", "Uttaranchal", "West Bengal", "No State"
+]
+
+REQUIRED_FIELDS = [
+    "user_role", "business_unit", "first_name", "last_name", "title", "phone", "email", "address1", "city", "country", "state"
+]
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import constants
 from auth import qualysApiAuth
 from xml_parser import qualysApiXmlParser
 from urllib.parse import quote
@@ -26,65 +27,13 @@ class qualysApiUser():
     ########################
     ### PUBLIC CONSTANTS ###
     ########################
-    COUNTRIES_WITH_STATES = ["United States of America", "Australia", "Canada", "India"]
-    VALID_COUNTRIES = [
-        "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda",
-        "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh",
-        "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia-Herzegovina",
-        "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria",
-        "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
-        "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia",
-        "Comoros", "Congo", "Cook Islands", "Costa Rica", "Cote D'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-        "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
-        "Equatorial Guinea", "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands (Malvinas)", "Fiji", "Finland",
-        "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia",
-        "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guatemala", "Guernsey, C.I.",
-        "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and McDonald Islands", "Honduras", "Hong Kong",
-        "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Isle of Man", "Israel",
-        "Italy", "Jamaica", "Japan", "Jersey, C.I.", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea", "Kuwait",
-        "Kyrgyzstan", "Lao Peoples Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia",
-        "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar",
-        "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius",
-        "Mexico", "Micronesia, Fed. States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco",
-        "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherland Antilles", "Netherlands",
-        "Neutral Zone (Saudi/Iraq)", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue",
-        "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama Canal Zone",
-        "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico",
-        "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Samoa", "San Marino",
-        "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovak Republic",
-        "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "St. Helena",
-        "St. Pierre and Miquelon", "St. Vincent and the Grenadines", "Sudan", "Suriname",
-        "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan",
-        "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago",
-        "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "U.S.Minor Outlying Islands",
-        "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay",
-        "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Vietnam", "Virgin Islands (British)",
-        "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe",
-    ]
-    VALID_US_STATES = [
-        "Alabama", "Alaska", "Arizona", "Arkansas", "Armed Forces Asia", "Armed Forces Europe", "Armed Forces Pacific",
-        "California", "Colorado", "Connecticut", "Delaware", "District of Columbia","Florida", "Georgia", "Hawaii",
-        "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
-        "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-        "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
-        "Pennsylvania", "Rhode Island","South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
-        "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-    ]
-    VALID_AUS_STATES = [
-        "New South Wales", "Northern Territory", "Queensland", "Tasmania", "Victoria", "Western Australia",
-    ]
-    VALID_CAN_STATES = [
-        "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland",
-        "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island", "Quebec",
-        "Saskatchewan", "Yukon"
-    ]
-    VALID_IN_STATES = [
-        "Andhra Pradesh", "Andaman and Nicobar Islands", "Arunachal Pradesh", "Assam", "Bihar",
-        "Chandigarh", "Chattisgarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana",
-        "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshadadweep",
-        "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Orissa", "Pondicherry",
-        "Punjab", "Rajasthan","Sikkim", "Tamil Nadu", "Tripura", "Uttar Pradesh", "Uttaranchal", "West Bengal", "No State"
-    ]
+    COUNTRIES_WITH_STATES = constants.COUNTRIES_WITH_STATES
+    VALID_COUNTRIES = constants.VALID_COUNTRIES
+    VALID_US_STATES = constants.VALID_US_STATES
+    VALID_AUS_STATES = constants.VALID_AUS_STATES
+    VALID_CAN_STATES = constants.VALID_CAN_STATES
+    VALID_IN_STATES = constants.VALID_IN_STATES
+    REQUIRED_FIELDS = constants.REQUIRED_FIELDS
 
     #######################
     ### PRIVATE OBJECTS ###
@@ -96,7 +45,6 @@ class qualysApiUser():
     ######################
     failed_users = []
     headers = {"X-Requested-With": "Python3Requests", "Content-Type": "application/x-www-form-urlencoded"}
-    required_fields = ["user_role", "business_unit", "first_name", "last_name", "title", "phone", "email", "address1", "city", "country", "state"]
     successful_users = []
 
     def __init__(self):
@@ -144,7 +92,7 @@ class qualysApiUser():
             Not all users will have the same details, but there are some required details that will stay the same.
 
             param: user_details
-            type: list
+            type: dictionary
             sample: {
                 user_role: "role", business_unit: "unit", first_name: "fname", last_name: "lname", title: "title",
                 phone: 1112223333, email: "name@domain.tld", address1: "1 Main Street", city: "Park Place",
