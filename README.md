83f70c4ce3c4aeea2f6c85f60cfcfafe06e6e883
Benjamin Owen <benjamin@projecttiy.com>
2023-09-29 13:31:03 -0400

    updated .gitignore to allow csv files to be committed as these now pertain to mailmerge, updated REAMDE.md with new instructions, removed all unnecessary variables from constants.py, updated necessary variables in constants.py, updated main.py to remove unnecessary functions dealing with reading in CSVs and validating headers, adjusted the create() function to use the new workflow, changed the program version in main.py, updated qualysApiUser to remove all unnecessary methods for validating country and state, updated qualysApiUser to remove all methods and workflows related to checking for duplicate users, updated addUser method in qualysApiUser to use the new workflow, updated parseUserReturn method in qualysApiXmlParser to pull  out the username and password data in the xml return, added in a helper class to centralize how the qualys api is called, added in sample mailmerge files

--- README.md
+++ README.md
@@ -37,11 +37,19 @@ api:
 
 You may want to update the base_url to match where your Qualys instance is
 
+## Configuration
+
+- mailmerge_server.conf: Please add at least one SMTP server configuration in this file for the `mailmerge` utility to work properly. There are examples for all types of SMTP servers present in the file and commented out, simply pick the example that best matches the SMTP server you are going to use.
+
+- mailmerge_template.txt: Please configure this template to match the email format you would like the `mailmerge` utility to send.
+
 ## Using
 
 There are two switches that are necessary to know:
-1. `-c/--create`: This will taken the given CSV file and create a user for each record in the CSV
-2. `-a/--create-and-tag`: This will take the given CSV file, create a user for each record in the CSV, create an asset tag using each user's username as the name, then apply the asset tag to the appropriate user, and apply the asset tag to a given asset/host
+1. `-c/--create`: This will taken the given `users.txt` file and create a user for each record in the CSV
+2. `-a/--create-and-tag`: This will take the given `users.txt` file, create a user for each record in the CSV, create an asset tag using each user's username as the name, then apply the asset tag to the appropriate user, and apply the asset tag to a given asset/host
+
+NOTE: Please ensure that each email address is on a separate line, one email address per line!
 
 To use `Qualys QSC Hands-on Training`, follow these steps:
 
@@ -51,10 +59,10 @@ To use `Qualys QSC Hands-on Training`, follow these steps:
 This will show you if your credentials are valid or need updating.
 
 - To create users, run the program like this:
-`python3 main.py --create --file /some/path/to/your/file.csv`
+`python3 main.py --create --file /some/path/to/your/users.txt`
 
 - To create and tag users, run the program like this:
-`python3 main.py --create-and-tag --file /some/path/to/your/file.csv`
+`python3 main.py --create-and-tag --file /some/path/to/your/users.txt`
 
 ## Contributing to Qualys QSC
 
--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import constants
+from helper import qualysApiHelper
 from auth import qualysApiAuth
 from xml_parser import qualysApiXmlParser
-from urllib.parse import quote
-import os, requests, time, xmltodict, yaml
+import time
 
 class qualysApiUser():
     """
@@ -21,193 +21,63 @@ class qualysApiUser():
     #########################
     ### PRIVATE CONSTANTS ###
     #########################
-    _PATH = os.path.dirname(os.path.realpath(__file__))
-    _CONFIG = _PATH + "/.creds.yaml"
 
     ########################
     ### PUBLIC CONSTANTS ###
     ########################
-    COUNTRIES_WITH_STATES = constants.COUNTRIES_WITH_STATES
-    VALID_COUNTRIES = constants.VALID_COUNTRIES
-    VALID_US_STATES = constants.VALID_US_STATES
-    VALID_AUS_STATES = constants.VALID_AUS_STATES
-    VALID_CAN_STATES = constants.VALID_CAN_STATES
-    VALID_IN_STATES = constants.VALID_IN_STATES
-    REQUIRED_FIELDS = constants.REQUIRED_FIELDS
 
     #######################
     ### PRIVATE OBJECTS ###
     #######################
-    _auth = ""
 
     ######################
     ### PUBLIC OBJECTS ###
     ######################
+    helper = ""
     headers = {"X-Requested-With": "Python3Requests", "Content-Type": "application/x-www-form-urlencoded"}
-    current_users = []
     failed_users = []
     successful_users = []
     users_to_tag = []
+    user_details = []
 
     def __init__(self):
-        # Instantiate the qualysApiAuth() class, which will run all the required authentication checks
-        # right out of the gate. If there are any problems authenticating, it will die right here.
-        # Otherwise, we continue as normal
-        self._auth = qualysApiAuth()
+        self.helper = qualysApiHelper()
 
-    def _callApi(self, endpoint, payload=None, request_type=None):
-        """
-            This method is here to make the network call to the Qualys API
-            and serves as a repeatable point of entry to that API.
-            param: endpoint
-            type: string
-            sampe: /path/to/api/endpoint
-
-            param: payload
-            type: string/dict
-            sample: param1=this&param2=that
-            sample: {'param1': 'this', 'param2': 'that'}
-
-            param: request_type
-            type: string
-            this is either "params" or "xml"
-
-            output: dict/boolean
-            result: either a dict from xmltodict library on success or
-                    False on failure
-        """
-        url = self._auth.SCHEME + self._auth.BASE_URL + endpoint
-        basic = requests.auth.HTTPBasicAuth(self._auth._username, self._auth._password)
-        if payload:
-            if request_type == "params":
-                r = requests.post(url=url, headers=self.headers, data=payload, auth=basic)
-            elif request_type == "xml":
-                self.headers["Content-Type"] = "text/xml"
-                r = requests.post(url=url, headers=self.headers, data=xmltodict.unparse(payload), auth=basic)
-        else:
-            r = requests.get(url=url, headers=self.headers, auth=basic)
-        if r.status_code == 200:
-            return r.text
-        else:
-            print(f"ERROR: Qualys API Call failed! URL: {url} :: Response Code: {r.status_code} :: Headers: {r.headers} :: Details: {r.text}")
-            return False
-
-    def validateCountryCode(self, country_code):
-        """
-            This method is used to validate if the Country Code assigned
-            to the User in the CSV file is a valid Country Code for the Qualys API
-            https://www.qualys.com/docs/qualys-api-vmpc-user-guide.pdf#G17.784098
-        """
-        if country_code in self.VALID_COUNTRIES:
-            return True
-        return False
-
-    def validateStateCode(self, country_code, state_code):
-        """
-            This method is used to validate if the State assigned to
-            the User in the CSV file is a valid State for the corresponding
-            Country Code for the Qualys API
-            https://www.qualys.com/docs/qualys-api-vmpc-user-guide.pdf#G17.784098
-        """
-        if country_code == "United States of America":
-            if state_code in self.VALID_US_STATES:
-                return True
-        elif country_code == "Australia":
-            if state_code in self.VALID_AUS_STATES:
-                return True
-        elif country_code == "Canada":
-            if state_code in self.VALID_CAN_STATES:
-                return True
-        elif country_code == "India":
-            if state_code in self.VALID_IN_STATES:
-                return True
-        return False
-
-    def listUsers(self):
-        """
-            https://docs.qualys.com/en/vm/api/users/index.htm#t=users%2Flist_users.htm
-            This method lists all users in the current subscription and
-            adds each email address to the self.current_users list which
-            will be used to verify if duplicate users are going to be created
-        """
-        endpoint = "/msp/user_list.php"
-        result = self._callApi(endpoint)
-        if result:
-            xml = qualysApiXmlParser(result)
-            for user in xml.xml_data["USER_LIST_OUTPUT"]["USER_LIST"]["USER"]:
-                email = user["CONTACT_INFO"]["EMAIL"]
-                if email not in self.current_users:
-                    self.current_users.append(email)
-        else:
-            print(f"ERROR: Unable to get user list!")
-            exit(1)
-
-    def addUser(self, user_details):
+    def addUser(self, user):
         """
             https://www.qualys.com/docs/qualys-api-vmpc-user-guide.pdf#G17.783431
             https://documenter.getpostman.com/view/7159960/SVzw51A9#52dc0ffe-a2ae-4dea-9cbb-661251477df5
-            This method takes in a dictionary of user_details and creates the user in the given Qualys subscription.
-            Not all users will have the same details, but there are some required details that will stay the same.
+            This method takes in an email address and creates the user in the
+            given Qualys Subscription. The only input is the email address, the rest comes from
+            the constants file.
 
-            param: user_details
-            type: dictionary
-            sample: {
-                user_role: "role", business_unit: "unit", first_name: "fname", last_name: "lname", title: "title",
-                phone: 1112223333, email: "name@domain.tld", address1: "1 Main Street", city: "Park Place",
-                country: "United States of America", state: "New York"
-            }
-
-            output: boolean
-            result: True/False
+            param: users
+            type: list
+            sample: test@test.com
         """
         action = "add"
-        send_email = 1
+        send_email = 0
         endpoint = "/msp/user.php"
         payload = f"action={action}&send_email={send_email}"
 
-        users = set(self.current_users)
-        if user_details["email"] in users:
-            print(f"ERROR: User's email address: {user_details['email']} is a duplicate!")
-            dup_user = f"{user_details['email']} {user_details['first_name']} {user_details['last_name']}"
-            self.failed_users.append(dup_user)
-            return False
-
-        # First, validate the Country Code
-        if not self.validateCountryCode(user_details["country"]):
-            print(f"ERROR: User has an invalid Country Code: {value}! Please fix their country value!")
-            self.failed_users.append(user_details["email"])
-            return False
-        else:
-            # Second, check if the Country Code requires a State
-            if user_details["country"] in self.COUNTRIES_WITH_STATES:
-                # Third, validate the State and that the State matches the Country
-                if not self.validateStateCode(user_details["country"], user_details["state"]):
-                    print(f"ERROR: User has an invalid State Code: {user_details['state']} for Country: {user_details['country']}! Please fix their state and/or country value(s)!")
-                    self.failed_users.append(user_details["email"])
-                    return False
-            else:
-                # Remove the State altogether if it's not needed
-                user_details.pop("state")
-        for key, value in user_details.items():
+        for key, value in constants.REQUIRED_FIELDS.items():
             payload = payload + f"&{key}={value}"
-            if key == "email":
-                email = value
-        result = self._callApi(endpoint, payload, "params")
+        result = self.helper.callApi(endpoint, payload, "params")
         if not result:
-            self.failed_users.append(email)
+            self.failed_users.append(user)
             return False
         else:
-            self.successful_users.append(email)
-            self.current_users.append(email)
+            self.successful_users.append(user)
             xml = qualysApiXmlParser(result)
-            username = xml.parseUserReturn()
-            if not username:
-                print(f"ERROR: Unable to create User {email}! URL: {url} :: Response Code: {r.status_code} :: Headers: {r.headers} :: Details: {xml.err_msg}")
-                self.failed_users.append(email)
-                return False
-            else:
-                self.users_to_tag.append(username)
-                return True
+            user_details = xml.parseUserReturn()
+            user_dict = {
+                "email": user,
+                "username": user_details[0],
+                "password": user_details[1],
+                "url": constants.URL
+            }
+            self.user_details.append(user_dict)
+            return True
 
     def searchUser(self, username):
         """
@@ -241,7 +111,7 @@ class qualysApiUser():
                 }
             }
         }
-        result = self._callApi(endpoint, payload, "xml")
+        result = self.helper.callApi(endpoint, payload, "xml")
         if not result:
             return -1
         else:
@@ -252,7 +122,7 @@ class qualysApiUser():
                     print(f"ERROR: Unable to locate the User's ID! This is probably because the user was just created, sleeping for 10s and trying again...")
                     print(xml.xml_data)
                     time.sleep(10)
-                    result = self._callApi(endpoint, payload, "xml")
+                    result = self.helper.callApi(endpoint, payload, "xml")
                     if not result:
                         return -1
                     else:
@@ -289,7 +159,7 @@ class qualysApiUser():
             }
         }
 
-        result = self._callApi(endpoint, payload, "xml")
+        result = self.helper.callApi(endpoint, payload, "xml")
         if result:
             xml = qualysApiXmlParser(result)
             role_result = xml.parseRoleAssignResult()
